ed07d63bd58e29250abbf524cf8535713b87d814
Zeihan Aulia <zeihan@warungpintar.co>
2020-12-06 06:26:11 +0700

    connecting to elasticsearch

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zeihanaulia/elasticsearch-with-go
 
 go 1.14
+
+require github.com/elastic/go-elasticsearch/v8 v8.0.0-20201202142044-1e78b5bf06b1
--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"strings"
 
+	"github.com/elastic/go-elasticsearch/v8"
+)
+
+func main() {
+	var r map[string]interface{}
+
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error creating client %s", err)
+	}
+
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+
+	if res.IsError() {
+		log.Fatalf("Error: %s", res.String())
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Fatalf("Error parsing the response body: %s", err)
+	}
+
+	log.Printf("Client: %s", elasticsearch.Version)
+	log.Printf("Server: %s", r["version"].(map[string]interface{})["number"])
+	log.Println(strings.Repeat("~", 37))
 }
